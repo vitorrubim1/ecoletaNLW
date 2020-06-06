ea00df7b2562d09633fb425a1847190bf0213cf0
Vitor <vitorrubim1@hotmail.com>
2020-06-06 18:50:42 -0300

    feat: Criado file de rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 
+//CONTROLLERS
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello world' });
-});
+//INSTANCIA DAS CLASSES
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+//itemsController
+routes.get('/items', itemsController.index); //LISTAGEM
+
+//pointsController
+routes.post('/points', pointsController.create); //CRIAÇÃO
+routes.get('/points/', pointsController.index); //LISTAR VÁRIOS
+routes.get('/points/:id', pointsController.show); //EXIBINDO COM ESPECIFICIDADE (COM REQUEST.PARAMS)
 
 export default routes;
