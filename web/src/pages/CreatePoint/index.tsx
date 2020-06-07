b0bd276f6227c782fc95fbd29c832bfcc1aba00c
Vitor <vitorrubim1@hotmail.com>
2020-06-07 18:46:50 -0300

    feat: cadastro de ponto de coleta finalizado

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, ChangeEvent } from 'react'; //ChangeEvent: MUDANÇA DE UM VALOR AONDE SEJA
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react'; //ChangeEvent: MUDANÇA DE UM VALOR AONDE SEJA, FormEvent: FORMULARIO
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
-import { LeafletMouseEvent } from 'leaflet';
+import { LeafletMouseEvent, Evented } from 'leaflet';
 import axios from 'axios';
 
 import api from '../../services/api';
@@ -37,10 +37,18 @@ const CreatePoint = () => {
 
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]); //LATITUDE E LONGITUDE, E DIGO Q VEM ARRAY DE NUMEROS
 
+    const [formData, setFormaData] = useState({
+        name: '',
+        email: '',
+        whatsapp: '',
+    });
 
     const [selectedUf, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
+    const [selectedItems, setSelectedItems] = useState<number[]>([]); //ARRAY DE NÚMEROS
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+    const history = useHistory();
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => { //POSIÇÃO INICIAL DO USUÁRIO, DA ONDE ELE ESTÁ
@@ -107,7 +115,7 @@ const CreatePoint = () => {
         setSelectedCity(city);
     }
 
-    //PRA ESCOLHER LOCALIDADE PELO MAPA
+    //PARA ESCOLHER LOCALIDADE PELO MAPA
     function handleMapCity(event: LeafletMouseEvent) {
         // console.log(event.latlng); //LATITUDE DE LONGITUDE QUE FOI CLICADA
 
@@ -115,6 +123,67 @@ const CreatePoint = () => {
             event.latlng.lat, //LATITUDE
             event.latlng.lng, //LONGITUDE
         ])
+    }
+
+    //PARA PEGAR INFORMAÇÕES DO INPUTS E SETAR EM UM ESTADO
+    function handleInputChange(event: ChangeEvent<HTMLInputElement>){
+        // console.log(event.target); 
+
+        const { name, value } = event.target; //NOME E VALOR DO INPUT
+
+        setFormaData({ ...formData, [name]: value }); //E AQ SETO O ESTADO, COPIANDO O VALOR Q JA TEM, INCLUINDO UM NOVO
+    }
+
+    function handleSelectItem(id: number){
+        // console.log("teste", id);        
+
+        const alreadySelected = selectedItems.findIndex(item => item === id);
+
+        if(alreadySelected >= 0){
+            //REMOVO O ITEM
+            const filteredItems = selectedItems.filter(item => item !== id);
+
+            setSelectedItems(filteredItems); //SETO O ESTADO SEM O ITEM QUE FOI DESELECIONADO
+        } else {
+            setSelectedItems([ ...selectedItems, id ]); //PARA PODER SELECIONAR MAIS DE UM ITEM, INCLUINDO AO QUE JA TEM
+        }
+
+        /*
+            PEGO TODO O ARRAY(state):'selectedItems',
+                'findIndex': RETORNA UM NÚMERO >= 0, SE JÁ TIVER DENTRO DO ARRAY, SE NÃO -1;
+                'item => item === id' EU PERCORRO O ARRAY, E VEJO SE TEM ALGUM ITEM IGUAL AO ID, QUE VEIO COMO PARAMETRO
+        */     
+    }
+
+    //CADASTRANDO INFORMAÇÕES
+    async function handleSubmit(event: FormEvent){
+        event.preventDefault();
+
+        //INFORMAÇÕES DE DENTRO DOS ESTADOS
+        const { name, email, whatsapp } = formData; 
+        const uf = selectedUf;
+        const city = selectedCity;
+        const [latitude, longitude] = selectedPosition;
+        const items = selectedItems;
+
+        const data = {
+            name,
+            email, 
+            whatsapp,
+            uf,
+            city,
+            latitude,
+            longitude,
+            items
+        }   
+
+        // console.log(data);
+
+        await api.post('points', data);
+       
+        alert('Ponto de coleta criado!!');
+
+        history.push('/');
     }
 
     return (
@@ -128,7 +197,7 @@ const CreatePoint = () => {
                 </Link>
             </header>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br />ponto de Coleta</h1>
 
                 <fieldset> {/*SEMÂNTICA*/}
@@ -142,6 +211,7 @@ const CreatePoint = () => {
                             type="text"
                             name="name"
                             id="name"
+                            onChange={handleInputChange}
                         />
                     </div>
 
@@ -152,6 +222,7 @@ const CreatePoint = () => {
                                 type="email"
                                 name="email"
                                 id="email"
+                                onChange={handleInputChange}
                             />
                         </div>
                         <div className="field">
@@ -160,6 +231,7 @@ const CreatePoint = () => {
                                 type="text"
                                 name="whatsapp"
                                 id="whatsapp"
+                                onChange={handleInputChange}
                             />
                         </div>
                     </div>
@@ -224,7 +296,11 @@ const CreatePoint = () => {
 
                     <ul className="items-grid">
                         {items.map(item => (
-                            <li key={item.id}>
+                            <li 
+                                key={item.id} 
+                                onClick={() => handleSelectItem(item.id)} 
+                                className={selectedItems.includes(item.id) ? 'selected': ''} //AQ EU VEJO SE NO ESTADO JA TEM O ID DO ITEM, SE JÁ TIVER SIDO, PONHO UMA CLASSE
+                            >
                                 <img src={item.image_url} alt={item.title} />
                                 <span>{item.title}</span>
                             </li>
