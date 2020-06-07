1be1e71ee77f0171fcbcf721ab075ada609ef8c0
Vitor <vitorrubim1@hotmail.com>
2020-06-07 15:59:28 -0300

    feat: Criado estrutura das páginas com estilização

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
+
+const Home = () => {
+    return (
+        <div id="page-home">
+            <div className="content">
+                <header>
+                    <img src={logo} alt="logo ecoleta" />
+                </header>
+
+                <main>
+                    <h1>Seu marketplace de coleta de resíduos</h1>
+                    <p>Ajudamos pessoa a encontrarem pontos de coleta de forma diferente.</p>
+
+                    <Link to="/create-point">
+                        <span>
+                            <FiLogIn />
+                        </span>
+                        <strong>Cadastre um ponto de coleta</strong>
+                    </Link>
+                </main>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
